7d9c4b942da35964306b933652a5162f5dedef58
David Gold <david.gold.a@gmail.com>
2016-09-21 14:54:48 -0700

    show support for JoinNodes

--- src/querynode/show.jl
+++ src/querynode/show.jl
@@ -1,16 +1,31 @@
 pad(io, n) = print(io, " "^n)
 
-function Base.show(io::IO, g::QueryNode, leftmargin=0, depth=0)
-    pad(io, min(leftmargin, 2))
-    println(typeof(g), " with arguments:")
-    for arg in g.args
-        pad(io, leftmargin+4); println(arg)
+function Base.show(io::IO, q::QueryNode, leftmargin=0, depth=0)
+    d = depth * 4
+    println(typeof(q))
+    pad(io, leftmargin+2+d); println("arguments:")
+    for (i, arg) in enumerate(q.args)
+        pad(io, leftmargin+6+d); println("$i)  ", arg)
     end
-    println()
-    pad(io, leftmargin); print("→→")
-    show(io, g.input, leftmargin+4, depth+1)
+    pad(io, leftmargin+2+d); println("inputs:")
+    pad(io, leftmargin+6+d); print(io, "1)  ")
+    show(io, q.input, leftmargin+6+d, depth+1)
+end
+
+function Base.show(io::IO, q::JoinNode, leftmargin=0, depth=0)
+    d = depth * 4
+    println(typeof(q))
+    pad(io, leftmargin+2+d); println("arguments:")
+    for (i, arg) in enumerate(q.args)
+        pad(io, leftmargin+6); println("$i)  ", arg)
+    end
+    pad(io, leftmargin+2+d); println("inputs:")
+    pad(io, leftmargin+6+d); print(io, "1)  ")
+    show(io, q.input1, leftmargin+6, depth+1)
+    pad(io, leftmargin+6+d); print(io, "2)  ")
+    show(io, q.input2, leftmargin+6, depth+1)
 end
 
 function Base.show(io::IO, d::DataNode, leftmargin, depth)
-    pad(io, 2); println("Data source: ", d.input)
+    println("Data source: ", d.input)
 end
